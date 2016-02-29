32f26806388c5d9a3ed8d1cdc41e6add2d62beb8
kosmonavtX <jawaman76@gmail.com>
2016-02-29 13:57:17 +0300

    Update fifo.h

--- fifo.h
+++ fifo.h
@@ -1,7 +1,7 @@
 #ifndef FIFO__H
 #define FIFO__H
  
-//размер должен быть степенью двойки: 4,8,16,32...128
+//СЂР°Р·РјРµСЂ РґРѕР»Р¶РµРЅ Р±С‹С‚СЊ СЃС‚РµРїРµРЅСЊСЋ РґРІРѕР№РєРё: 4,8,16,32...128
 #define FIFO( size )\
   struct {\
     unsigned char buf[size];\
@@ -9,38 +9,38 @@
     unsigned short head;\
   } 
  
-//количество элементов в очереди
+//РєРѕР»РёС‡РµСЃС‚РІРѕ СЌР»РµРјРµРЅС‚РѕРІ РІ РѕС‡РµСЂРµРґРё
 #define FIFO_COUNT(fifo)     (fifo.head-fifo.tail)
  
-//размер fifo
+//СЂР°Р·РјРµСЂ fifo
 #define FIFO_SIZE(fifo)      ( sizeof(fifo.buf)/sizeof(fifo.buf[0]) )
  
-//fifo заполнено?
+//fifo Р·Р°РїРѕР»РЅРµРЅРѕ?
 #define FIFO_IS_FULL(fifo)   (FIFO_COUNT(fifo)==FIFO_SIZE(fifo))
  
-//fifo пусто?
+//fifo РїСѓСЃС‚Рѕ?
 #define FIFO_IS_EMPTY(fifo)  (fifo.tail==fifo.head)
  
-//количество свободного места в fifo
+//РєРѕР»РёС‡РµСЃС‚РІРѕ СЃРІРѕР±РѕРґРЅРѕРіРѕ РјРµСЃС‚Р° РІ fifo
 #define FIFO_SPACE(fifo)     (FIFO_SIZE(fifo)-FIFO_COUNT(fifo))
  
-//поместить элемент в fifo
+//РїРѕРјРµСЃС‚РёС‚СЊ СЌР»РµРјРµРЅС‚ РІ fifo
 #define FIFO_PUSH(fifo, byte) \
   {\
     fifo.buf[fifo.head & (FIFO_SIZE(fifo)-1)]=byte;\
     fifo.head++;\
   }
  
-//взять первый элемент из fifo
+//РІР·СЏС‚СЊ РїРµСЂРІС‹Р№ СЌР»РµРјРµРЅС‚ РёР· fifo
 #define FIFO_FRONT(fifo) (fifo.buf[fifo.tail & (FIFO_SIZE(fifo)-1)])
  
-//уменьшить количество элементов в очереди
+//СѓРјРµРЅСЊС€РёС‚СЊ РєРѕР»РёС‡РµСЃС‚РІРѕ СЌР»РµРјРµРЅС‚РѕРІ РІ РѕС‡РµСЂРµРґРё
 #define FIFO_POP(fifo)   \
   {\
       fifo.tail++; \
   }
  
-//очистить fifo
+//РѕС‡РёСЃС‚РёС‚СЊ fifo
 #define FIFO_FLUSH(fifo)   \
   {\
     fifo.tail=0;\
